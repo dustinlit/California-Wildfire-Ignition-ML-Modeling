cf294787af4339477e69f6159fdf0572f3246d03
Dustin Littlefield <dustinlit@gmail.com>
2025-12-18 23:58:57 -0800

    Archive old files

--- notebooks/Version 4.0/app.py
+++ notebooks/Version 4.0/app.py
@@ -49,5 +49,11 @@ style={
 # -------------------------
 # Run server
 # -------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=False,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8050))
+    )
